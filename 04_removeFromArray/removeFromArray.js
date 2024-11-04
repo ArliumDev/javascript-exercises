77fcdd4d54e039b162305f966d7a5f774e6e5ff1
ArliumDev <arliumdev@gmail.com>
2024-11-04 12:41:38 +0000

    some exercises redone

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...valuesToRemove) {
+  const removed = arr.filter(item => !valuesToRemove.includes(item));
+  return removed;
 };
 
 // Do not edit below this line
